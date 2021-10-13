8e786792ccf8c22e8bc38a261d29157a069ef424
BobBudowniczy <mateuszgorczyca6@gmail.com>
2021-10-13 19:02:45 +0200

    Added docstring to newly created functions.

--- Task 1/fire.py
+++ Task 1/fire.py
@@ -70,14 +70,25 @@ class Fire:
         for i in filenames:
             os.remove(i)
 
-    def find_biggest_cluster(self, plot=False):
-        """if plot=True then the the final and labeled matrix will be printed"""
+    def find_biggest_cluster(self, plot: bool = False) -> int:
+        """Function is labeling and finding the biggest cluster is self.lattice
+
+        :argument
+        plot: bool
+        > if True then the the final, labeled matrix and biggest cluster size will be printed
+        > default: False
+
+        :return
+        biggest_cluster: int
+        > size of biggest cluster in self.lattice"""
+
+        # labeling
         largest_label = 0
         label = np.zeros((self.L, self.L))
         for P in product(range(self.L), repeat=2):
             P_label = self.lattice[P[0]][P[1]]
-            L = (P[0]-1, P[1])
-            U = (P[0], P[1]-1)
+            L = (P[0] - 1, P[1])
+            U = (P[0], P[1] - 1)
             if P_label == 3:
                 if L[0] >= 0 and self.lattice[L[0]][L[1]] == 3:
                     L_label = label[L[0]][L[1]]
@@ -97,11 +108,12 @@ class Fire:
 
         # counting the biggest cluster size
         biggest_cluster = 0
-        for num in range(1, largest_label+1):
+        for num in range(1, largest_label + 1):
             cluster_size = np.sum(label == num)
             if cluster_size > biggest_cluster:
                 biggest_cluster = cluster_size
 
+        # plotting if desired
         if plot:
             print("The forest at the end:")
             print(self.lattice)
@@ -121,7 +133,10 @@ class Fire:
 
         :return
         fire_to_the_other_end_count: float
-        > probability that the fire will go to the other side of a grid."""
+        > probability that the fire will go to the other side of a grid.
+        biggest_clusters: float
+        > the average size of biggest cluster of burned trees after the simulation is finished.
+        """
 
         fire_to_the_other_end_count = 0
         biggest_clusters = 0
