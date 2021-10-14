898b704f5bc941c6dd74c261bdaf081928ee3e71
BobBudowniczy <mateuszgorczyca6@gmail.com>
2021-10-14 16:43:40 +0200

    Added multiprocessing to drawing curves.

--- Task 1/plot_of_next_side.py
+++ Task 1/plot_of_next_side.py
@@ -1,6 +1,14 @@
 from fire import Fire
 import matplotlib.pyplot as plt
 import numpy as np
+import multiprocessing as mp
+
+
+def help_func(args):
+    L, p = args
+    fire = Fire(L, p)
+    print("Doing for p = ", p)
+    return fire.do_MC(50)
 
 
 def plot_of_next_side(L: int):
@@ -10,33 +18,55 @@ def plot_of_next_side(L: int):
     :argument
     L: int
     > the length of the side of the grid"""
-    chances = []
-    clusters = []
-    ps = [1/50 * p for p in range(51)]
-    for p in ps:
-        fire = Fire(L, p)
-        chance, cluster = fire.do_MC(50)
-        chances.append(chance)
-        clusters.append(cluster)
+
+    # chances = []
+    # clusters = []
+    ps = [1 / 50 * p for p in range(51)]
+    args = [[L, 1 / 50 * p] for p in range(51)]
+
+    # for p in ps:
+    #     fire = Fire(L, p)
+    #     chance, cluster = fire.do_MC(50)
+    #     chances.append(chance)
+    #     clusters.append(cluster)
+
+    pool = mp.Pool()
+    result = np.array(pool.map(help_func, args))
+    pool.close()
+    pool.join()
+    chances = result[:, 0]
+    clusters = result[:, 1]
+
     start = 0
     stop = 50
     for i in range(50):
-        if chances[i] != chances[i+1]:
+        if chances[i] != chances[i + 1]:
             start = i
             break
     for i in range(50):
-        if chances[50-i-1] != chances[50-i]:
-            stop = 50-i
+        if chances[50 - i - 1] != chances[50 - i]:
+            stop = 50 - i
             break
     p_start = ps[start]
     p_stop = ps[stop]
     ps2 = np.linspace(p_start, p_stop, 40)
-    chances2, clusters2 = [], []
+    args2 = []
     for p in ps2:
-        fire = Fire(L, p)
-        chance, cluster = fire.do_MC(50)
-        chances2.append(chance)
-        clusters2.append(cluster)
+        args2.append([L, p])
+    # chances2, clusters2 = [], []
+    # for p in ps2:
+    #     fire = Fire(L, p)
+    #     chance, cluster = fire.do_MC(50)
+    #     chances2.append(chance)
+    #     clusters2.append(cluster)
+
+    pool = mp.Pool()
+    result = np.array(pool.map(help_func, args2))
+    pool.close()
+    pool.join()
+    chances2 = result[:, 0]
+    clusters2 = result[:, 1]
+
     ps = [*ps[:start], *ps2, *ps[stop:]]
     chances = [*chances[:start], *chances2, *chances[stop:]]
     clusters = [*clusters[:start], *clusters2, *clusters[stop:]]
