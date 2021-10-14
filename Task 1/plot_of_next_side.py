968e07fba933ce52c30dcfad3ebe7d5c11f5c4e9
BobBudowniczy <mateuszgorczyca6@gmail.com>
2021-10-14 19:26:41 +0200

    Changed layout of plots

--- Task 1/plot_of_next_side.py
+++ Task 1/plot_of_next_side.py
@@ -82,34 +82,42 @@ def plot_of_next_side(L: int, fname1: str = "plot1.pdf", fname2: str = "plot2.pd
     # getting biggest slope
     chances_MA_max_diff = 0
     chances_MA_max_diff_i = 0
-    for i in range(len(chances2)-1):
-        chances_MA_diff = chances_MA[i+1] - chances_MA[i]
+    for i in range(len(chances2) - 1):
+        chances_MA_diff = chances_MA[i + 1] - chances_MA[i]
         if chances_MA_diff > chances_MA_max_diff:
             chances_MA_max_diff = chances_MA_diff
             chances_MA_max_diff_i = i
-    chances_MA_max_diff_p = (ps2[chances_MA_max_diff_i] + ps2[chances_MA_max_diff_i+1]) / 2
-    chances_MA_max_diff_chance = (chances_MA[chances_MA_max_diff_i] + chances_MA[chances_MA_max_diff_i+1]) / 2
+    chances_MA_max_diff_p = (ps2[chances_MA_max_diff_i] + ps2[chances_MA_max_diff_i + 1]) / 2
+    chances_MA_max_diff_chance = (chances_MA[chances_MA_max_diff_i] + chances_MA[chances_MA_max_diff_i + 1]) / 2
     ps2_diff = ps2[1] - ps2[0]
 
     chances_MA_max_diff_p_star = chances_MA_max_diff_p - ps2_diff * chances_MA_max_diff_chance / chances_MA_max_diff
 
     # plotting
     plt.figure(figsize=(12, 8))
-    plt.plot(ps, chances, color="b")
-    plt.plot(ps2, chances2, color="r")
-    plt.plot(ps2, chances_MA, color="g")
-    plt.plot([chances_MA_max_diff_p, chances_MA_max_diff_p_star], [chances_MA_max_diff_chance, 0], marker="*")
-    plt.title('Probability that fire hits the opposite edge for different p')
-    plt.xlabel('p')
-    plt.ylabel('q') # q - fraction of simulations where fire hits the opposite edge
+    plt.plot(ps, chances, color="#7777FF", label="normal precision")
+    plt.plot(ps2, chances2, color="#FFCCCC", label="higher precision")
+    plt.plot(ps2, chances_MA, color="green", label="MA(5)")
+    plt.plot([chances_MA_max_diff_p, chances_MA_max_diff_p_star],
+             [chances_MA_max_diff_chance, 0],
+             "--", marker="o", color="green", label="counting of $p^*$")
+    plt.text(chances_MA_max_diff_p_star + 0.02,
+             0,
+             "$p^*\\approx$" + " %.2f" % chances_MA_max_diff_p_star,
+             verticalalignment='center', fontsize=12)
+    plt.title('Probability that fire hits the opposite edge for different p', fontsize=18)
+    plt.xlabel('p', fontsize=12)
+    plt.ylabel('q', fontsize=12)  # q - fraction of simulations where fire hits the opposite edge
+    plt.legend()
     plt.savefig(fname1)
     plt.show()
     plt.figure(figsize=(12, 8))
-    plt.plot(ps, clusters, color="b")
-    plt.plot(ps2, clusters2, color="r")
-    plt.title('Average highest cluster of burnt trees for different p')
-    plt.xlabel('p')
+    plt.plot(ps, clusters, color="b", label="normal precision")
+    plt.plot(ps2, clusters2, color="r", label="higher precision")
+    plt.title('Average highest cluster of burnt trees for different p', fontsize=18)
+    plt.xlabel('p', fontsize=12)
     plt.ylabel('Average highest cluster')
+    plt.legend()
     plt.savefig(fname2)
     plt.show()
-    print("Max change os ")
+    print("Max change is ", chances_MA_max_diff_p_star)
