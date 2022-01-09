7b809c9662ff9daa0bce7761aa1508bd64c3c06f
probak97 <234816@student.pwr.edu.pl>
2022-01-09 21:54:10 +0100

    Added plot for different f

--- Task 7/model2.py
+++ Task 7/model2.py
@@ -3,46 +3,47 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 class SituationModel:
-    def __init__(self, n, T, f):
+    def __init__(self, n, T):
         self.n = n
         self.T = T
-        self.f = f
         self.g = nx.complete_graph(n)
-    def step(self, p):
+    def step(self, p, f):
         chosen = np.random.choice(self.g.nodes)
         if np.random.rand()<p:
-            if np.random.rand()<self.f:
+            if np.random.rand()<f:
                 self.current_states[chosen] *= -1
         else:
             neighbors = np.random.choice(list(self.g.neighbors(chosen)),4,False)
             neighbors_states = self.current_states[neighbors]
             if np.abs(np.sum(neighbors_states))==4:
                 self.current_states[chosen] = neighbors_states[0]
-    def simulate(self, p):
+    def simulate(self, p, f):
         self.current_states = np.ones(self.n)
         for t in range(self.T):
-            self.step(p)
+            self.step(p, f)
         return np.sum(self.current_states==1)/self.n
-    def mc(self, nmc, p):
+    def mc(self, nmc, p, f):
         c = np.zeros(nmc)
         for mc in range(nmc):
-            c[mc] = self.simulate(p)
+            c[mc] = self.simulate(p, f)
         return np.mean(c)
-    def plot1(self, p_vector, nmc):
-        c_vector = np.zeros(p_vector.shape)
-        for i, p in enumerate(p_vector):
-            print(f'doing for p={p}')
-            c_vector[i] = self.mc(nmc, p)
-            
-        plt.figure(figsize=(12,8))    
-        plt.plot(p_vector, c_vector, 'o-')
+    def plot1(self, p_vector, f_vector, nmc, fname='plot.pdf'):
+        c_vector = np.zeros((len(f_vector),len(p_vector)))
+        plt.figure(figsize=(12,8)) 
+        for i, f in enumerate(f_vector):
+            for j, p in enumerate(p_vector):
+                c_vector[i,j] = self.mc(nmc, p, f)
+           
+            plt.plot(p_vector, c_vector[i,:], 'o-', label=f'f={f:.2}')
         plt.xlabel('p')
         plt.ylabel('c')
+        plt.title('Concentration of adopted c in the stationary state as a function of independence p for the situation model)
+        plt.savefig(fname)
         plt.show()
         return c_vector
 
 if __name__ == "__main__":
-    
-    model = SituationModel(100, 10000, 0.5)
+    model = SituationModel(100, 10000)
     ps = np.arange(0,1.05,0.05)
-    cs = model.plot1(ps,50)
+    fs = np.arange(0.2,0.6,0.1)
+    cs = model.plot1(ps,fs,100)
