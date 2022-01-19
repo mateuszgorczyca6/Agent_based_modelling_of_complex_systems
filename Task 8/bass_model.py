cadf15023bfbf2f3412c13ccbd6d4724a8597e98
BobBudowniczy <mateuszgorczyca6@gmial.com>
2022-01-19 11:57:25 +0100

    List 8 - fixed approach

--- Task 8/bass_model.py
+++ Task 8/bass_model.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 import numpy as np
 from matplotlib import pyplot as plt
 from numpy.random import rand
@@ -9,27 +7,22 @@ class Person:
     def __init__(self):
         self.state = False
 
+    def update(self, p, q, innovation_ratio):
+        U = rand()
+        if not self.state and U < p + q * innovation_ratio:
+            self.state = True
+            if U < p:
+                return 1, 0  # innovator
+            return 0, 1  # imitator
+        return 0, 0  # nothing
+
     def __int__(self):
         return int(self.state)
 
 
-class Innovator(Person):
-    def update(self, p, *_):
-        if rand() < p:
-            self.state = True
-            return 1, 0
-        return 0, 0
-
-
-class Imitator(Person):
-    def update(self, _, q, innovation_ratio):
-        if rand() < q * innovation_ratio:
-            self.state = True
-            return 0, 1
-        return 0, 0
-
-
 class Bass_Model:
+    __Person = Person
+
     def __init__(self,
                  p_coefficient_of_innovation: float,
                  q_coefficient_of_imitation: float,
@@ -40,16 +33,10 @@ class Bass_Model:
         self.q = q_coefficient_of_imitation
         self.N = number_of_nodes
         self.persons = self.__set_initial_types()
-        print(self.persons)
 
     def __set_initial_types(self) -> np.ndarray:
-        persons = deque([])
-        for person in range(self.N):
-            if rand() < self.p:
-                persons.append(Innovator())
-            else:
-                persons.append(Imitator())
-        return np.array(persons)
+        persons = np.array([Person() for _ in range(self.N)])
+        return persons
 
     def update(self):
         ratio = self.__calculate_ratio()
@@ -58,7 +45,8 @@ class Bass_Model:
             new_p, new_q = person.update(self.p, self.q, ratio)
             new_ps += new_p
             new_qs += new_q
-        return ratio, new_ps, new_qs
+        new_all = new_ps + new_qs
+        return ratio, new_ps, new_qs, new_all
 
     def __calculate_ratio(self):
         return self.persons.astype(int).mean()
@@ -68,17 +56,19 @@ class Bass_Model:
 
 
 if __name__ == "__main__":
-    model = Bass_Model(0.03, 0.38, 1000)
-    ratios, ps, qs = [], [], []
+    model = Bass_Model(0.03, 0.38, 10000)
+    ratios, ps, qs, all_news = [], [], [], []
     for _ in range(50):
-        ratio, p, q = model.update()
+        ratio, p_new, q_new, all_new = model.update()
         ratios.append(ratio)
-        ps.append(p)
-        qs.append(q)
+        ps.append(p_new)
+        qs.append(q_new)
+        all_news.append(all_new)
 
     plt.plot(ratios)
     plt.show()
 
     plt.plot(ps)
     plt.plot(qs)
+    plt.plot(all_news)
     plt.show()
